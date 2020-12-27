864931db06ee51f761a4b05c2e9c0fe207813966
spfave <sebpfave@gmail.com>
2020-12-27 17:11:42 -0500

    get destination data in main

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from data_manager import DataManager
+from pprint import pprint
+
+
+# Main
+flight_data = DataManager()
+
+flight_destination_data = flight_data.get_destination_data()["prices"]
+pprint(flight_destination_data)
