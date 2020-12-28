b8fdbf69e2c6eb023c72fe666e17f79f991b0404
spfave <sebpfave@gmail.com>
2020-12-28 00:37:49 -0500

    query flight method refactor

--- flight_search.py
+++ flight_search.py
@@ -2,6 +2,7 @@ import os
 import requests
 from dotenv import load_dotenv
 from datetime import date, timedelta
+from flight_data import FlightData
 from pprint import pprint
 
 from requests.models import Response
@@ -24,42 +25,22 @@ class FlightSearch:
     """ This class is responsible for talking to the Flight Search API. """
 
     def __init__(self):
-        pass
+        self.headers = {"apikey": kiwi_key,}
 
     def query_city_code(self, search_city):
         search_params = {
-            "apikey": kiwi_key,
             "term": search_city,
             "location_types": "airport",
         }
-        response = requests.get(url=kiwi_loc, params=search_params)
+        response = requests.get(url=kiwi_loc, params=search_params, headers=self.headers)
         response.raise_for_status()
 
         location_data = response.json()["locations"]
         city_code = location_data[0]["city"]["code"]
         return city_code
 
-    def query_flight(self, departure_loc, arrival_loc, **kwargs):
-
-        date_travel_start = kwargs.get("date_travel_start", date.today()+timedelta(days=1))
-        date_travel_end = kwargs.get("date_travel_end", date.today()+timedelta(days=180))
-
-        flight_params = {
-            "apikey": kiwi_key,
-            "fly_from": departure_loc,
-            "fly_to": arrival_loc,
-            "date_from": date_travel_start.strftime("%d/%m/%Y"),
-            "date_to": date_travel_end.strftime("%d/%m/%Y"),
-        }
-        if "nights_min" in kwargs and "nights_max" in kwargs:
-            flight_params["nights_in_dst_from"] = kwargs.get("nights_min")
-            flight_params["nights_in_dst_to"] = kwargs.get("nights_max")
-        if "currency" in kwargs:
-            flight_params["curr"] = kwargs.get("currency") 
-        if "price_max" in kwargs:
-            flight_params["price_to"] = kwargs.get("price_max")
-        
-        response = requests.get(url=kiwi_search, params=flight_params)
+    def query_flight(self, flight_search_params):
+        response = requests.get(url=kiwi_search, params=flight_search_params, headers=self.headers)
         response.raise_for_status()
 
         return response.json()
@@ -70,5 +51,6 @@ class FlightSearch:
 if __name__ == "__main__":
     # pass
     fs = FlightSearch()
-    data = fs.query_flight(DEPARTURE_CITY, "PAR", currency="USD", price_max=199)
+    fd = FlightData(DEPARTURE_CITY, "PAR", currency="USD", price_max=199)
+    data = fs.query_flight(fd.flight_params)
     pprint(data)
