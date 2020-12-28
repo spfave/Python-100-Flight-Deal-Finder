1785d4f68cabcc539b8687619aed67ebd100db50
spfave <sebpfave@gmail.com>
2020-12-27 22:07:07 -0500

    get city iata code

--- flight_search.py
+++ flight_search.py
@@ -1,3 +1,43 @@
+import os
+import requests
+from dotenv import load_dotenv
+from pprint import pprint
+load_dotenv()
+
+
+# Constants
+URL_KIWI = "https://tequila-api.kiwi.com"
+
+
+# Variables
+kiwi_key = os.getenv("KIWI_KEY")
+kiwi_loc = f"{URL_KIWI}/locations/query"
+
+
+# Classes
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+    """ This class is responsible for talking to the Flight Search API. """
+
+    def __init__(self):
+        pass
+
+    def query_city_code(self, search_city):
+        search_params = {
+            "term": search_city,
+            "location_types": "airport",
+            "apikey": kiwi_key,
+        }
+        response = requests.get(url=kiwi_loc, params=search_params)
+        response.raise_for_status()
+
+        location_data = response.json()["locations"]
+        city_code = location_data[0]["city"]["code"]
+        return city_code
+
+
+# Main
+if __name__ == "__main__":
+    # pass
+    fs = FlightSearch()
+    data = fs.query_city_code("Paris")
+    pprint(data)
