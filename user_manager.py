c2ead3c903689d1bb3b59aa822411b69befc8bd9
spfave <sebpfave@gmail.com>
2021-01-03 13:06:50 -0500

    User management

--- user_manager.py
+++ user_manager.py
@@ -14,19 +14,6 @@ auth_flight_data = (os.getenv("SHEETY_AUTH_USERNAME"),
 
 
 # Classes
-class UserManager():
-    """  """
-
-    def __init__(self):
-        pass
-
-    def new_user(self):
-        pass
-
-    def add_user(self, user):
-        pass
-
-
 class User():
     """  """
 
@@ -36,16 +23,41 @@ class User():
         self.get_email()
 
     def get_email(self):
-        self.email1 = input("Enter your email: ")
-        self.email2 = input("Enter your email again: ")
-        self.validate_email()
+        email1 = input("Enter your email: ")
+        email2 = input("Enter your email again: ")
+        self.validate_email(email1, email2)
+        self.email = email1
 
-    def validate_email(self):
-        if self.email1 != self.email2:
+    def validate_email(self, email1, email2):
+        if email1 != email2:
             print("\nYour email entries don't match, please enter them again")
             self.get_email()
 
 
+class UserManager():
+    """  """
+
+    def __init__(self):
+        pass
+
+    def new_user(self):
+        user = User()
+        self.add_user(user)
+
+    def add_user(self, user: User):
+        user_data = {
+            "user": {
+                "firstName": user.first_name,
+                "lastName": user.last_name,
+                "email": user.email,
+            }
+        }
+        response = requests.post(
+            url=URL_USERS_DATA, json=user_data, auth=auth_flight_data)
+        response.raise_for_status()
+
+
 # Main
 if __name__ == "__main__":
-    user = User()
+    um = UserManager()
+    um.new_user()
